db877c1a331a34a68afa2ac7b287610c88779bea
twissi <ich@kristin-albert.de>
2014-04-06 19:13:26 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BostonChallenge::Application.config.secret_key_base = '57c9fb108d6618f2da8ad722863bb438954598217bfc42e0b6a9c38b9ba76299c618c919843210ff62558a7d031443c32c3f1e828f4165685d195225b5fec383'
