a72a2b7df9ac599407fe55d60cb11e6efd556179
Michael Emhofer <emi@fsinf.at>
2014-04-08 23:13:07 +0200

    added helper methods to user for policies

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :rememberable, :trackable, :validatable
+
+  def ben?
+    self.role == 'ben'
+  end
+
+  def admin?
+    self.role == 'admin'
+  end
 end
