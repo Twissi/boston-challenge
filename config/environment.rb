7b10cf89dc6b850c80af79e84fed6800d3a2a824
twissi <ich@kristin-albert.de>
2014-04-13 21:23:04 +0200

    Update challenge date format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 BostonChallenge::Application.initialize!
+
+Time::DATE_FORMATS[:day_and_month] = '%a, %d.%m.'
